c5abf4b7319757994ced2799e05317f26a9c7802
aidata <aidata@10.121.3.198>
2022-10-27 10:34:33 +0300

    Reversearray

--- src/arays/dizi.java
+++ src/arays/dizi.java
@@ -1,5 +1,29 @@
 package arays;
 
 public class dizi {
+	
+	public static void main(String[]args){
+	int length=9;
+	int[] array = {1,2,3,4,5,6,7,8,9,10};
+	
+	System.out.printf("%s%8s%n", "Index","Value");
+	
+	for (int counter=0; counter < 10; counter++) {
+		System.out.printf("%5d%8d%n", counter,array[counter] );
+	}
+		for (int counter=0; counter < 5; counter++) {	
+			int temp=0;
+			temp=array[length];
+			array[length]=array[counter];
+			array[counter]=temp;
+			length--;
+			
+	}
+		for(int i=0;i<10;i++) {
+			System.out.printf("%5d%8d%n",i,array[i]);
+		}
+	}
+	
+	
 
 }
